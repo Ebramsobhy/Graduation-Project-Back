551d26a44b1e27b36679a75783c5778f706b5608
Salama-Malek <salamahassanein@gmail.com>
2023-06-17 20:03:45 +0300

    updated the files

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# This the URL of the Application Front-End 
-https://github.com/Ebramsobhy/Graduation-Project-Front
+# Caricature_Website_Back
---
+++ routes/all_users/registration_routes.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const router = express.Router();
+const upload = require("../../middleware/multer");
+const { register } = require("../../controllers/registeration/register");
+const { login } = require("../../controllers/registeration/login");
+const { getAllArtists, getArtistById } = require("../../controllers/admin_contollers/artists.controller");
+const { getAuthorById, getAllAuthors } = require("../../controllers/admin_contollers/authors.controller");
+const { getAllCaricatures, getCaricatureById, getCaricatureByCharacterId } = require("../../controllers/admin_contollers/caricatures.controller");
+const { getAllCharacters, getCharactersByAuthorOrArtist } = require("../../controllers/admin_contollers/characters.controller");
+const { contactUs } = require("../../controllers/contactus/contactus");
+const loginMiddleware = require("../../middleware/login_middleware");
+const { createFavouriteCaricature } = require("../../controllers/favourite_caricature/create_favourite_caricature");
+
+//=============Register==========================================
+router.post("/register", upload.single("image"), register);
+
+//=============Login===========================================
+router.post("/login", login);
+
+/****GET ALL ARTIST AND GET ONE BY ID****/
+router.get('/artist', getAllArtists);
+router.get('/artist/:id', getArtistById);
+
+/*****************************************/
+
+/****GET ALL AUTHORS AND GET ONE BY ID****/
+router.get('/authors', getAllAuthors);
+router.get('/author/:id', getAuthorById);
+
+/*****************************************/
+
+
+/****GET ALL CARICATURE AND GET ONE BY ID****/
+router.get('/caricatures', getAllCaricatures);
+router.get('/caricatures/character/:id', getCaricatureByCharacterId);
+router.get('/caricature/:id', getCaricatureById);
+
+/*****************************************/
+
+
+/****GET ALL CHARACTARES AND GET ONE BY ID****/
+router.get('/characters', getAllCharacters);
+router.get('/character/:id', getCharactersByAuthorOrArtist);
+
+/*****************************************/
+
+
+/****GET ALL CHARACTARES AND GET ONE BY ID****/
+router.get('/contact', contactUs);
+
+/*****************************************/
+
+
+/****Add favourite caricature****/
+router.post('/caricature/:caricatureid/userid/:id',loginMiddleware, createFavouriteCaricature);
+
+/*****************************************/
+
+
+
+module.exports = router;
+
+
+
